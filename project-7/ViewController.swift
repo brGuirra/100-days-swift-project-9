6713eb3fb058777868e9c787c5cb1d6e516cc518
brGuirra <br.guirra@gmail.com>
2022-01-20 20:26:02 -0300

    feat: add the basic components in the UI
    Add UITabBarController, TableViewController and UINavigationController

--- project-7/ViewController.swift
+++ project-7/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var petitions = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Petitions app"
     }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
 
+        content.text = "Title goes here"
+        content.secondaryText = "Subtitle goes here"
+
+        cell.contentConfiguration = content
+
+        
+        return cell
+    }
 }
 
